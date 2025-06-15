fdcec3a5d313dcc9124c7416a006026fab135b70
Mdusman398 <maanlive398@gmail.com>
2025-06-15 16:32:11 +0500

    updated

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 let emails = +prompt("How much accounts you want to make ");
 let userEmails = [];
 let userPasswords = [];
-for (i = 1; i <= emails; i++ ){
-    let emails = prompt(`Enter the Email ${i}`);
-    let password= prompt(`Enter the Password ${i}`);
-    userEmails.push(emails);
-    userPasswords.push(password);
-    console.log(userEmails );
-    console.log(userPasswords);
-    
+alert(
+  "Kindly enter your email address  make sure it's in the correct format like yourname@example.com with the '@' symbol in it."
+);
+for (i = 1; i <= emails; i++) {
+  let emails = prompt(`Enter the Email ${i} `);
+  let password = prompt(`Enter the Password ${i}`);
+  userEmails.push(emails);
+  userPasswords.push(password);
+  alert(`Account ${i} successfully added 🎊 `);
+  console.log(userEmails);
+  console.log(userPasswords);
 }
-alert("Now which account do you want to login !")
-let userEmail = prompt("enter the email you want to login ");
-let userpass = prompt("enter your password");
+document.write(
+  " <h1> Here is your Emails : </h1> <br /> <h2> " + userEmails + " </h2> <br/>"
+);
+document.write(
+  "<h1> Here is your Emails Password : </h1> <br /> <h2> " +
+    userPasswords +
+    " </h2> <br />"
+);
+// alert("Now which account do you want to login !")
+let userEmail = prompt(
+  "Enter the Email and Log in to any one of the accounts you have created"
+);
+let userpass = prompt("Enter the password");
 let isCorrect = false;
-for (let i = 0; i <= userEmails.length; i++){
-    if(userEmails[i] === userEmail && userPasswords[i] === userpass){
-        isCorrect = true;
-        alert("Login Successfully! 😍");
-        document.write("Login Successfully! 😍 wellcome to Website ")  
-    }
+for (let i = 0; i <= userEmails.length; i++) {
+  if (userEmails[i] === userEmail && userPasswords[i] === userpass) {
+    isCorrect = true;
+    alert("Login Successfully! 😍");
+    document.write(" <h1> Login Successfully! 😍 wellcome to Website </h1>");
+    document.write("<h2> You are currently logged in with this account : </h2> " + " <br> " + "<h3>" + "( " + userEmails[i] + " )" + " </h3>")
+  }
 }
-if (isCorrect === false){
-    alert("please enter valid Email and passwaord! 😐");
+if (isCorrect === false) {
+  alert("Email and Password doesn't match 😐");
 }
 
-alert(" All of your Emails and passwords are shown in the console");
-alert("Thank you🥰")
+// alert(" All of your Emails and passwords are shown in the console");
+// alert("Thank you🥰")
